8d75c015da042c88425d0a2e096bcdec50d39c6c
satoshi terahara <triple.zero.option@gmail.com>
2016-01-30 09:17:49 +0000

    Setting...

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
+  resource :settings, only: [:edit, :update]
   resources :users, only: [:index, :show]
   
   root to: 'registrations#new'
